68def430357c7425622fc4a88f8523f3f1a39fe2
Madis Kapsi <madis@scr.ee>
2020-03-30 00:57:49 +0300

    Updates to use via composer

--- src/API.php
+++ src/API.php
@@ -120,7 +120,10 @@ class API extends \Infira\MeritAktiva\General
 		
 		$curl = curl_init($url);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-		curl_setopt($curl, CURLOPT_HTTPHEADER, ["Content-type: application/json"]);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, [
+			"Content-type: application/json",
+			"Content-Length: " . strlen($json),
+		]);
 		curl_setopt($curl, CURLOPT_POST, TRUE);
 		if ($json)
 		{
